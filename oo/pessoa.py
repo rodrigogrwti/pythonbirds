9773c52cb2963eeb4a4a2ac92fffc901b792b643
Rodrigo Silva <rodrigo.grwti@gmail.com>
2020-08-29 13:53:37 -0300

    Criados atritudos de instacias (nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
+    def __init__(self, nome = None, idade=33):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-#    print(Pessoa.cumprimentar(p))
+    p = Pessoa('Ana')
+    print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Rodrigo'
+    print(p.nome, p.idade)
